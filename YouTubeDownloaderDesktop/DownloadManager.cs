e67ce1ab1745f6e2a55feeacf3721a4aa16c254d
akcorp2003 <kuanga2016@ucla.edu>
2016-10-13 00:25:26 -0700

    Added files
    Began implementation.

---
+++ YouTubeDownloaderDesktop/DownloadManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using VideoLibrary;
+
+namespace YouTubeDownloaderDesktop
+{
+    public class DownloadManager
+    {
+        public void SaveVideo(string link)
+        {
+            YouTube youTube = YouTube.Default;
+            var video = youTube.GetVideo(link);
+
+            File.WriteAllBytes(@"C:\Users\Aland\Documents" + video.FullName, video.GetBytes());
+
+            MessageBox.Show("It's finished!");
+
+        }
+    }
+}
